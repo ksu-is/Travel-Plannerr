a28738fa6a8989fbbf9b8f00319cd0377b3fc57f
Felipa <herrera.79532@gmail.com>
2023-11-26 18:50:09 -0500

    Adding cal event def

--- travelplan.py
+++ travelplan.py
@@ -1,3 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import calendar
+
+#add a calander event
+def add_event():
+    selected_date = cal.get_date()
+    event = event_entry.get()
+    if selected_date and event:
+        event_calendar.insert(
+            " ",
+            "end"
+            values=(selected_date,event))
+        event_entry.delete(0,"end")
+        
+        
