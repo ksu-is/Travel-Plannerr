7878895297209bf1ebdd29b7fbe108500769b127
Felipa <herrera.79532@gmail.com>
2023-11-26 18:59:36 -0500

    creating def for event deletion

--- travelplan.py
+++ travelplan.py
@@ -11,6 +11,10 @@ def add_event():
             " ",
             "end"
             values=(selected_date,event))
-        event_entry.delete(0,"end")
-        
-        
+        event_entry.delete(0,"end") 
+
+#delete selected event
+def delete_event():
+    selected_item= event_calendar.seletion()
+    if selected_item:
+        event_calendar.delete(selected_item)        
